5949b82886a2aeeff4eddbfd479038218807da48
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2023-05-20 14:42:45 +0530

    3rd commit

--- src/Components/TaleRow.js
+++ src/Components/TaleRow.js
@@ -11,7 +11,7 @@ let TableRow = ({name, symbol, current_price,fully_diluted_valuation,market_cap,
             <td>${current_price}</td>
             <td >${fully_diluted_valuation}</td>
             <td className={price_change_percentage>0? "pos": "neg"}>{price_change_percentage}%</td>
-            <td>${market_cap}</td>
+            <td>Mkt Cap: ${market_cap}</td>
         </tr>
     )
 };
